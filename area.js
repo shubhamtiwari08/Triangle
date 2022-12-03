4ef3dfdd98b77d125a56927a6998ee7b0041c79b
shubhamtiwari08 <st1173076@gmail.com>
2022-12-03 20:36:21 +0530

    added basic logic and html for area

--- area.js
+++ area.js
@@ -1,6 +1,7 @@
 var base = document.querySelector("#base")
 var height = document.querySelector("#height")
 var findButton = document.querySelector("#find-btn")
+var result = document.querySelector("#result")
 
 
 findButton.addEventListener("click", area)
@@ -9,6 +10,16 @@ findButton.addEventListener("click", area)
 
 function area() {
 
-    console.log("clicked")
+    let baseValue = Number(base.value)
+    let heightValue = Number(height.value)
+
+
+    let area = ((baseValue * heightValue) / 2)
+
+    result.textContent = area
+
+
+
 
 }
+
