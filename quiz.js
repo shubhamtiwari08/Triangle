c573b03ce7605dd2c834a8e9ff94e4a1924f3826
shubhamtiwari08 <st1173076@gmail.com>
2022-12-06 02:03:57 +0530

    added questions and logic

--- quiz.js
+++ quiz.js
@@ -1,29 +1,34 @@
-var quizForm = document.querySelectorAll(".quiz-form")
-var submitButton = document.querySelector("#submit-btn")
-var result = document.querySelector("#result")
-
-correctAnswers = ["90", "right angled", "one right angle", "3,4,5", "25°", "10°", "a+b+c", "0", "45°,45°"]
 
 
+const quizform = document.querySelector(".quiz-form");
+const submitBtn = document.querySelector("#submit-answers-btn");
+const outputEl = document.querySelector(".output");
 
+const correctAnswers = [
+    "90°",
+    "right angled",
+    "one right angle",
+    "12, 16, 20",
+    "Equilateral triangle",
+    "100°",
+    "30°",
+    "a + b + c",
+    "no",
+    "45°",
+];
 
-function Marks() {
-    // let score = 0
-    // let index = 0
-    // const formResults = new FormData(quizForm)
-    // for (let value of formResults.values()) {
-    //     if (value === correctAnswers[index]) {
-    //         score = score + 1
-    //     }
-
-    //     index = index + 1
-
-
-    // }
-
-    console.log("clicked")
-
-    // result.textContent = "Your score is :" + score
+function calculateScore() {
+    const formResults = new FormData(quizform);
+    let score = 0,
+        index = 0;
+    for (let value of formResults.values()) {
+        if (value === correctAnswers[index]) {
+            score = score + 1;
+        }
+        index = index + 1;
+    }
+    // console.log("The score is "+score);
+    outputEl.innerText = "The score is " + score;
 }
 
-submitButton.addEventListener("click", Marks)
+submitBtn.addEventListener("click", calculateScore);
